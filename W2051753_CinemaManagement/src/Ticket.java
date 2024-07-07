7d08b5d016fdc07f0884131996594f5b2ab3d746
Kavindu Vidushan Karunathilaka <143307455+karuna3laka@users.noreply.github.com>
2024-07-07 21:20:06 +0530

    mistake update

--- W2051753_CinemaManagement/src/Ticket.java
+++ W2051753_CinemaManagement/src/Ticket.java
@@ -1,13 +1,54 @@
 public class Ticket {
     private int row;
     private int price;
-    private int Seat;
+    private int seat;
     private Person person;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        seat = seat;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 
     public Ticket(int row, int price, int seat, Person person) {
         this.row = row;
         this.price = price;
-        this.Seat = seat;
+        this.seat = seat;
         this.person = person;
+    }
+
+    public void printTicketInfo() {
+        System.out.println("Ticket Information:");
+        System.out.println("Row: " + row);
+        System.out.println("Seat: " + seat);
+        System.out.println("Price:" + price);
+        System.out.println("Person Information:");
+        person.printPersonInfo();
     }
 }
